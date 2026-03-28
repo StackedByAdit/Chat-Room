9381eec311690ab0cd0581e847bdc1cd814572e4
Aditya <adityaguptavns11@gmail.com>
2026-03-29 00:23:51 +0530

    added ping pong ws

--- chat-room/src/components/MessageBox.tsx
+++ chat-room/src/components/MessageBox.tsx
@@ -1,22 +1,54 @@
-import React, {useRef} from 'react'
+  import React, {useEffect, useRef, useState} from 'react'
 
-export const MessageBox = () => {
-  const inputMessage = useRef<HTMLInputElement| null>(null);
+  export const MessageBox = () => {
+    const inputMessage = useRef<HTMLInputElement| null>(null);
+      const [socket, setSocket] = useState<WebSocket | null>(null);
 
-  function handleInput() {
-    if(!inputMessage.current){
-      return;
+    function handleInput() {
+      if(!inputMessage.current){
+        return;
+      }
+
+      if (!inputMessage.current || !socket) return;
+
+      console.log(inputMessage.current.value)
+      
+      const message = inputMessage.current?.value;
+      socket.send(message);
+
     }
-    console.log(inputMessage.current.value)
-  }
-  return (
-    <div>
-        <div style={{display:'flex', gap:10}}>
-        <input ref={inputMessage} style={{padding:10, borderRadius:15, boxShadow: '2px 2px 5px red, -2px -2px 5px blue', width:"90%"}} type='text' placeholder='say Hello'></input>
-        <button onClick={handleInput} style={{padding:1, borderRadius:15, boxShadow: '2px 2px 5px red, -2px -2px 5px blue', width:"10%" }}>Send</button>
-        </div>
-    </div>
-  )
-}
 
-export default MessageBox
+
+  useEffect(() => {
+      const ws = new WebSocket("ws://localhost:6969");
+      setSocket(ws);
+
+      ws.onopen = () => {
+        console.log("Connected");
+      };
+
+      ws.onmessage = (e) => {
+        console.log("Received:", e.data);
+      };
+
+      ws.onclose = () => {
+        console.log("Disconnected");
+      };
+
+      return () => {
+        ws.close();
+      };
+    }, []);
+
+    
+    return (
+      <div>
+          <div style={{display:'flex', gap:10}}>
+          <input ref={inputMessage} style={{padding:10, borderRadius:15, boxShadow: '2px 2px 5px red, -2px -2px 5px blue', width:"90%"}} type='text' placeholder='say Hello'></input>
+          <button onClick={handleInput} style={{padding:1, borderRadius:15, boxShadow: '2px 2px 5px red, -2px -2px 5px blue', width:"10%" }}>Send</button>
+          </div>
+      </div>
+    )
+  }
+
+  export default MessageBox
