228f5fb042417dc1e55228355eeba4150d1e5040
Aditya <adityaguptavns1@gmail.com>
2026-03-30 03:30:24 +0530

    many to one room logic

--- backend/server.ts
+++ backend/server.ts
@@ -1,14 +1,41 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
-const wss = new WebSocketServer({ port: 6969 });
+const wss = new WebSocketServer({port : 6969});
+
+interface User{
+    socket: WebSocket;
+    room: string
+}
+
+let allSockets : User[] = [];
 
 wss.on("connection", (socket) => {
-  console.log("Client connected");
+    
+    socket.on("message", (message : string) => {
+    
+        const parsedMessage = JSON.parse(message);
 
-  socket.on("message", (message) => {
-    console.log("Received from client:", message.toString());
-    console.log("test");
+        if(parsedMessage.type === "join"){
+           
+            allSockets.push({
+                socket,
+                room : parsedMessage.payload.roomId
+            })
+        }
 
-    socket.send(message.toString());
-  });
-});
+        if (parsedMessage.type  === "chat"){
+        
+            let currentUserRoom = null;
+            for(let i =0; i<allSockets.length; i++){
+                if(allSockets[i].socket === socket){
+                    currentUserRoom = allSockets[i].room
+                }
+            }
+            for(let i =0; i<allSockets.length; i++){
+                if(allSockets[i].room === currentUserRoom){
+                    allSockets[i].socket.send(parsedMessage.payload.message)
+                }
+            }
+        }
+    })
+})
