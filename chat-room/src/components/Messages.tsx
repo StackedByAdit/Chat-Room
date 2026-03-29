bd294dd68087a12a2ebaa6ba67878f2df5eb9253
Aditya <adityaguptavns11@gmail.com>
2026-03-29 15:15:17 +0530

    ws connection

--- chat-room/src/components/Messages.tsx
+++ chat-room/src/components/Messages.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Messages = () => {
+    const [displayMessages, setdisplayMessages] = useState<string[]>([])
+
+    useEffect(() => {
+        const ws = new WebSocket("ws://localhost:6969");
+
+        ws.onopen = () => {
+            console.log("Connected");
+        };
+
+        ws.onmessage = (e) => {
+            console.log("Received:", e.data);
+            setdisplayMessages(m => [...m, e.data])
+
+        };
+
+        ws.onclose = () => {
+            console.log("Disconnected");
+        };
+
+        return () => {
+            ws.close();
+        };
+    }, []);
+
+
+
     return (
-        <div style={{height:"65%", color:"white"}}>
-            Messages
+        <div style={{ height: "65%", color: "white" }}>
+            <div style={{padding : 10, width:100, display:"flex", flexDirection:"column", backgroundColor:"white", color:"black"}}>
+                {displayMessages.map(m => <div>
+                    {m}
+                </div>)}
+            </div>
         </div>
     )
 }
