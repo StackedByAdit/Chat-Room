cb6b825ee8dae671983a210e945f84105db72e3c
Aditya <adityaguptavns1@gmail.com>
2026-04-12 23:55:55 +0530

    made changes

--- chat-room/src/components/RIghtMargin.tsx
+++ chat-room/src/components/RIghtMargin.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const RightMargin = () => {
     return (
         <div style={{height:"100vh", width:"20%", borderLeft: "1px solid rgba(192, 192, 192, 0.15)", color:"white"}}>
-            
+            <div>Talk to People worldwide</div>
         </div>
     )
 }
