17cfae93fbd4cc458b4b78f8e14155737e6f1438
Aditya <adityaguptavns1@gmail.com>
2026-06-04 17:04:31 +0530

    cleanup

--- chat-room/src/components/LeftMargin.tsx
+++ chat-room/src/components/LeftMargin.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export const LeftMargin = () => {
   return (
     <div style={{height:"100vh", width:"20%", borderRight: "1px solid rgba(238, 238, 237, 0.08)", color:"white"}}>
